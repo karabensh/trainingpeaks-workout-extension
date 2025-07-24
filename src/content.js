8a5a7121b895ce1f726bdf410eb1d57e1d0a88a0
Nate Karabensh <natekarabensh@gmail.com>
2025-07-24 09:28:55 -0400

    Support All Workout Block Types
    - Adds support for all workout block types
    - Creates new src/constants directory for constants
    - Creates workout-block-types.js to house constants for the different block type values
    - Updates src/content.js to use the new constants, and chooses a new block type to click on each button press

--- src/content.js
+++ src/content.js
@@ -1,12 +1,20 @@
 import browser from "webextension-polyfill";
 import { clickWorkoutBlockByTitle } from "./workout";
+import * as WorkoutBlockTypes from './constants/workout-block-types.js';
 
 console.log("Content script active");
+
+var count = 0;
 
 browser.runtime.onMessage.addListener((message) => {
   console.log("Received message:", message);
   console.log("Received message:", message);
   if (message.action === "clickWarmup") {
-    clickWorkoutBlockByTitle("Warm up");
+
+    clickWorkoutBlockByTitle(
+      WorkoutBlockTypes.BLOCK_TYPE_LIST[count% WorkoutBlockTypes.BLOCK_TYPE_LIST.length]
+      , document
+    );
+    count++;
   }
 });
